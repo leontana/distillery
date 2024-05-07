28085c5516bb054135d906c7dd179e6e08678e73
leontana <126888853+leontana@users.noreply.github.com>
2024-05-07 12:03:43 +0300

    Update erts.sh

--- priv/libexec/erts.sh
+++ priv/libexec/erts.sh
@@ -325,7 +325,7 @@ USAGE
 
 # Run IEx
 iex() {
-    elixir --no-halt --erl "-noshell -user Elixir.IEx.CLI" +iex "$@"
+    elixir --no-halt --erl "-noshell -user" +iex "$@"
 }
 
 # Echoes the current ERTS version
